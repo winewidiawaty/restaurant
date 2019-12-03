882a8d381542c560345bf86e3310ef3d221b721d
winewidiawaty <widiawatywine@gmail.com>
2019-12-03 15:35:32 +0700

    latihan praktikum mopro modul restaurant

---
+++ app/src/main/java/com/wine/restaurant/data/Constans.java
@@ -0,0 +1,9 @@
+package com.wine.restaurant.data;
+
+public class Constans {
+    ///Restoran
+    public static final String WEB_URL_RESTORAN = "http://www.pakartanaman.com/skripsi/public/";
+    public static final String REGISTER = WEB_URL_RESTORAN + "register/";
+    public static final String LOGIN = WEB_URL_RESTORAN + "login/";
+}
+
